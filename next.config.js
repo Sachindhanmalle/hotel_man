cc85e366944f498eed9bc19a2d3e7742418cd7f4
C H PRABHU KISHOR <187014358+Kishor237323@users.noreply.github.com>
2026-05-13 18:38:13 +0530

    Remove images configuration from nextConfig
    Removed unoptimized images configuration from nextConfig.

--- next.config.js
+++ next.config.js
@@ -1,14 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  images: {
-    unoptimized: true,
-  },
-}
-
-
-
-/** @type {import('next').NextConfig} */
-const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
